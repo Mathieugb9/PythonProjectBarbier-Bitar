0b59d6aac55b77207df1510e7009a8acf7250bfd
Mathieu <mathieu.barbier@efrei.net>
2023-11-26 23:54:07 +0100

    Projet et function combiné des deux pc

--- functions.py
+++ functions.py
@@ -29,8 +29,7 @@ def get_president_lastname():
     set_withoutdouble = set(president_name)
     president_name_nodouble = list(set_withoutdouble)
     return president_name_nodouble
-def get_president_entirename():
-    listname = get_president_lastname()
+def get_president_entirename(listname): #Must use get_president_lastname
     List_fullname = []
 
     for name in listname:
@@ -74,7 +73,7 @@ def convert_file_lowercases():
     return()
 
 
-def delete_all_ponctuation():
+def delete_all_ponctuation():#must have converted in lowercase first
     file_list = get_list_of_clean_files()
     punctuation = '!"#$%&()*+,./:;<=>?@[\\]^_`{|}~'
 
@@ -97,25 +96,22 @@ def delete_all_ponctuation():
 
 #TF-FUNCTIONS :
 
-def tf_function(file_name): #Il faut modifier pour qu'on puisse choisir qu'elle fichier est le bon
-
-    filepath = f'/Users/mathieu/PycharmProjects/PYTHONPROJECTGPT/Cleaned/{file_name}'
+def tf_function(file_name):
+    filepath = source_path = os.path.join('Cleaned', file_name)
     with open(filepath,'r') as file:
         lines = file.readlines()
         word_count = {}
         for line in lines:
-
             words = line.split()
             for word in words:
                 word_count[word] = word_count.get(word, 0) + 1
     return(word_count)
 
-#IDF FUNCTION :
-def idf_function(): #modify so it can only take a directory as parameter ( use the function ) and be giving a dictionary and not an list
+#IDF FUNCTION:
+def idf_function():
     file_list = get_list_of_clean_files()
     N = len(file_list)
     word_document_count = {}
-
 
     for file_name in file_list:
         word_counts = tf_function(file_name)
@@ -124,14 +120,192 @@ def idf_function(): #modify so it can only take a directory as parameter ( use t
                 word_document_count[word] = 0
             word_document_count[word] += 1
 
-
-    IDFLIST = []
+    idf_dict = {}
     for word in word_document_count:
-        idf_score = math.log(N / word_document_count[word])
-        L = [word, idf_score]
-        IDFLIST.append(L)
+        idf_dict[word] = math.log(N / word_document_count[word])
 
-    return IDFLIST
-'''
-def TF_IDF_function():
-'''
+    return idf_dict
+
+def TF_IDF_function_matrix():#it print the matrix in the inverse ,the first document is in the last column of the matrix
+    file_list = get_list_of_clean_files()
+    idf_dictionary = idf_function()
+    tf_idf_dict = {}
+    for file_name in file_list:
+        word_count = tf_function(file_name)
+        for word, tf in word_count.items():
+            if word not in tf_idf_dict:
+                tf_idf_dict[word] = [0] * len(file_list)
+            file_index = file_list.index(file_name)
+            tf_idf_dict[word][file_index] = tf * idf_dictionary.get(word, 0)
+    tf_idf_matrix = []
+    for word in tf_idf_dict:
+        row = tf_idf_dict[word]
+        tf_idf_matrix.append(row)
+    return tf_idf_matrix
+def floor_to_three_decimals(number):
+    return math.floor(number * 1000) / 1000
+def TF_IDF_function_with_names():
+    file_list = get_list_of_clean_files()
+    idf_dictionary = idf_function()
+    tf_idf_dict = {}
+    for file_name in file_list:
+        word_count = tf_function(file_name)
+        for word, tf in word_count.items():
+            if word not in tf_idf_dict:
+                tf_idf_dict[word] = [0] * len(file_list)
+            file_index = file_list.index(file_name)
+            tf_idf_dict[word][file_index] = floor_to_three_decimals(tf * idf_dictionary.get(word, 0))
+    file_list_name_only=[]
+    for names in file_list:
+        file_list_name_only.append(names[11:-12])
+    header_row = ["word/document"] + file_list_name_only
+    tf_idf_matrix = [header_row]
+    for word in tf_idf_dict:
+        row = [word] + tf_idf_dict[word]
+        '''row = tf_idf_dict[word] # if we just want the matrix'''
+        tf_idf_matrix.append(row)
+    return tf_idf_matrix
+
+def get_list_of_least_important_words(matrix):
+    ListofIDF0words = []
+    for row in matrix[1:]:
+        IDF_0 = True
+        for element in row[1:]:
+            if (element != 0) :
+                IDF_0 = False
+        if (IDF_0 == True) :
+            ListofIDF0words.append(row[0])
+
+    return(ListofIDF0words)
+
+def get_list_of_highest_TFIDF_score(matrix):
+    List_of_Highest_TFIDF_score=[]
+    for row in matrix[1:] :
+        for element_index in range(1,len(row)):
+            element = row[element_index]
+            if (element >= 2.7):
+                word = row[0]
+                document_name = matrix[0][element_index]
+                List_of_Highest_TFIDF_score.append((word, document_name, element))
+    return(List_of_Highest_TFIDF_score)
+
+
+def get_list_of_most_repeated_words_by_chirac(N):
+    Chirac_count = {}
+    List_of_file_by_chirac = ["Nomination_Chirac1_cleaned.txt","Nomination_Chirac2_cleaned.txt"]
+    for file in List_of_file_by_chirac:
+        word_count = tf_function(file)
+        for word, count in word_count.items():
+            if word not in Chirac_count:
+                Chirac_count[word] = 0
+            Chirac_count[word] += count
+    sorted_dict = sorted(Chirac_count.items(), key=lambda x: x[1], reverse=True) #function found to sort a dictionary
+    for i in range(N):
+        print(sorted_dict[i])
+    return ()
+'''def get_president_lastname_Cleaned_for_Nation(input_list):
+    president_name = []
+    for string in input_list:
+        president_lastname = string[10:-11]
+        filtered_string = ''.join([char for char in president_lastname if char.isalpha() or char.isspace()])
+        president_name.append(filtered_string)
+    set_withoutdouble = set(president_name)
+    president_name_nodouble = list(set_withoutdouble)
+    return president_name_nodouble'''
+def President_Nation_word():
+    list=get_list_of_clean_files()
+    List_of_names_of_files_containing_Nation = []
+
+    original_dict = {}
+    for element in list:
+        dict = tf_function(element)
+        if "nation" in dict:
+            List_of_names_of_files_containing_Nation.append(element)
+            original_dict[element] = dict["nation"]
+    '''print(original_dict)'''
+    new_dict = {}
+    for key, value in original_dict.items():
+        filtered_string = key[10:-11]
+        cleaned_key = ''.join(filter(str.isalpha, filtered_string))
+        if cleaned_key in new_dict:
+            new_dict[cleaned_key] += value
+        else:
+            new_dict[cleaned_key] = value
+    '''return new_dict,List_of_names_of_files_containing_Nation'''
+    return new_dict
+
+def repeated_the_most_time(dictionary,N):
+    sorted_dict = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)  # function found to sort a dictionary
+    if N >= len(sorted_dict):
+        N = len(sorted_dict)
+    for i in range(N):
+        print(sorted_dict[i])
+    return ()
+
+def get_first_president_speaking_climat_ecologie():
+    file_list = get_list_of_clean_files()
+    dict_climat = {}
+    dict_ecologie = {}
+    found_climat = False
+    found_écologie = False
+    for file_path in file_list:
+        source_path = os.path.join('Cleaned', file_path)
+        with open(source_path, 'r') as file:
+            iteration = 0
+            lines = file.readlines()
+            for line in lines:
+                if "climat" in line and file_path not in dict_climat:
+                    dict_climat[file_path] = iteration
+                    found_climat = True
+                elif "écologie" in line and file_path not in dict_ecologie:
+                    dict_ecologie[file_path] = iteration
+                    found_écologie = True
+                iteration += 1
+
+    if found_climat == True :
+        key_cleaned_dict_climat = {}
+        for key, value in dict_climat.items():
+            filtered_string = key[10:-11]
+            cleaned_key_climat = ''.join(filter(str.isalpha, filtered_string))
+            key_cleaned_dict_climat[cleaned_key_climat]= value
+        sorted_dict_climat = sorted(key_cleaned_dict_climat.items(), key=lambda x: x[1], reverse=True)
+        print("The first president to talk about climat is (Name,Line)",sorted_dict_climat[1])
+
+    if found_écologie == True:
+        key_cleaned_dict_ecologie = {}
+        for key, value in dict_ecologie.items():
+            filtered_string = key[10:-11]
+            cleaned_key_climat = ''.join(filter(str.isalpha, filtered_string))
+            key_cleaned_dict_climat[cleaned_key_climat] = value
+        sorted_dict_ecologie = sorted(key_cleaned_dict_ecologie.items(), key=lambda x: x[1], reverse=True)
+        print("The first president to talk about écologie is (Name,Line)",sorted_dict_ecologie[1])
+    elif found_climat == False :
+        print("climat not found in files.")
+    elif found_écologie == False :
+        print("écologie not found in files")
+
+
+
+
+
+    return()
+def get_list_of_words_all_president_said(matrix):
+    List_of_TFIDF_greater_than_zero=[]
+    appear_in_doc = False
+    for row in matrix[1:]:
+        if all(element > 0 for element in row[1:5]) and (row[5] > 0 or row[7] > 0) and (row[6] > 0 or row[8] > 0):
+            word = row[0]
+            List_of_TFIDF_greater_than_zero.append(word)
+
+    '''list_unimportant = get_list_of_least_important_words(matrix)
+    List_of_grzero_not_in_unimportant = []
+    for element in List_of_TFIDF_greater_than_zero :
+        similarity = False
+        for values in list_unimportant:
+            if element == values :
+                similarity == True
+        if similarity == False :
+            List_of_grzero_not_in_unimportant.append(element)'''
+
+
+    return(List_of_TFIDF_greater_than_zero)
--- main.py
+++ main.py
@@ -1,3 +1,126 @@
 from functions import *
-list = idf_function()
-print(list)
+import sys
+import os
+
+def print_matrix(matrix): #web found function to print on pycharm a large matrix in a proper way for the eyes
+    for row in matrix:
+        for element in row:
+            print(f"{element:20}", end="")
+        print()
+
+def printspaces():
+    print("\n")
+
+def main():
+        GREEN = '\033[92m'
+        WHITE = '\033[97m'
+        RESET = '\033[0m'
+
+        print(GREEN + "\n*********************************")
+        print("      MY FIRST CHATBOT")
+        print("*********************************" + RESET)
+        print(WHITE + "\n      Project Python" + RESET)
+        print(WHITE + "\n   BITAR RALPH BARBIER MATHIEU" + RESET)
+        print(WHITE + "\n      Promo 2028 int-2" + RESET)
+        input("Press Enter to continue...")
+        while True:
+            print("\nMain Menu:")
+            print("1. Print the list of files")
+            print("2. Start the cleaning of files")
+            print("3. Print the TF-IDF matrix")
+            print("4. Print the list of least important words")
+            print("5. Print the list of words with the highest TF-IDF score")
+            print("6. Print the most repeated words by a specific president")
+            print("7. Print the word 'nation' repeated the most times")
+            print("8. Find the first president speaking about 'climat' and 'écologie'")
+            print("9. Print the list of words said by all presidents")
+            print("0.Exit ")
+            printspaces()
+
+            choice = input("Enter the number corresponding to the function you want to access: ")
+
+            if choice == '1':
+                list = get_list_of_files()
+                printspaces()
+                print(list)
+                printspaces()
+                input("To return to the menu press any key :")
+
+
+            elif choice == '2':
+                input("Press Enter to convert the files to lowercase .( All the file will be added to the Cleaned directory)")
+                convert_file_lowercases()
+                printspaces()
+                input("Press Enter to delete the punctuation of the files")
+                delete_all_ponctuation()
+                printspaces()
+                choice_f = input("PRESS ENTER TO RELAUNCH THE APP TO CREATE THE NEW CLEANED FILES")
+                break
+
+
+            elif choice == '3':
+                printspaces()
+                print("1. Print the TF-IDF matrix with names")
+                print("2. Print the TF-IDF matrix only the grid")
+                choiceIDFTF = input("Enter the number corresponding to the function you want to access: ")
+                if choiceIDFTF == "1":
+                    idftfname = TF_IDF_function_with_names()
+                    print_matrix(idftfname)
+                elif choiceIDFTF == "2":
+                    idftf = TF_IDF_function_matrix()
+                    print_matrix(idftf)
+                else:
+                    while choiceIDFTF != 0:
+                        print(("Invalid choice. Please enter a valid number:"))
+                        choiceIDFTF = input("Enter the number corresponding to the function you want to access: ")
+                input("To return to the menu press any key :")
+                printspaces()
+
+            elif choice == '4':
+                printspaces()
+                matrix = TF_IDF_function_with_names()
+                List_impr = get_list_of_least_important_words(matrix)
+                print(List_impr)
+                input("To return to the menu press any key :")
+                printspaces()
+            elif choice == '5':
+                printspaces()
+                matrix = TF_IDF_function_with_names()
+                List_impr2 = get_list_of_highest_TFIDF_score(matrix)
+                print(List_impr2)
+                input("To return to the menu press any key :")
+                printspaces()
+            elif choice == '6':
+                printspaces()
+                N = int(input("Enter an integer for the integer-most said words by chirac : "))
+                ListChr = get_list_of_most_repeated_words_by_chirac(N)
+                print(ListChr)
+                input("To return to the menu press any key :")
+                printspaces()
+            elif choice == '7':
+                printspaces()
+                dictnation = President_Nation_word()
+                repeated_the_most_time(dictnation,4)
+                input("To return to the menu press any key :")
+                printspaces()
+            elif choice == '8':
+                printspaces()
+                get_first_president_speaking_climat_ecologie()
+                input("To return to the menu press any key :")
+                printspaces()
+            elif choice == '9':
+                printspaces()
+                print(get_list_of_words_all_president_said(TF_IDF_function_with_names()))
+                input("To return to the menu press any key :")
+                printspaces()
+
+            elif choice == '0':
+                print("Exiting the application.")
+                break
+
+            else:
+                print("Invalid choice. Please enter a valid number.")
+
+
+if __name__ == "__main__":
+    main()
